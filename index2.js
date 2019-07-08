66b90e88e46fb07f58f76ed44e43f4221b9d87fb
vbc221 <vbc221@nyu.edu>
2019-07-08 16:21:14 -0500

    problem 2

--- index2.js
+++ index2.js
@@ -1,3 +1,18 @@
 function jediName(firstName, lastName) {
   return  lastName.slice(0,3) + firstName.slice(0,2)
 }
+
+function beyond(num){
+  if (num===Infinity||num===Number.NEGATIVE_INFINITY){
+    console.log('And Beyond')
+  }
+  if (Infinity>num>0){
+    console.log('To Infinity')
+  }
+  if (0>num>Number.NEGATIVE_INFINITY){
+    console.log('To Negative Infinity')
+  }
+  if (num===0){
+    console.log('Staying Home')
+  }
+}
