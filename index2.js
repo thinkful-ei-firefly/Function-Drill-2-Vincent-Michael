b62becce6f2fe62ac5048e7af612387258f4bdd9
m-cal <iengineersoftware@gmail.com>
2019-07-09 09:14:09 -0400

    finished how many days in a month

--- index2.js
+++ index2.js
@@ -64,3 +64,37 @@ for (let i = 0; i < example.length; i++) {
 }
 
 console.log(result);
+
+
+// How many days in a month
+
+function howManyDays(month, leapYear) {
+  let result;
+  switch (month) {
+  case 'January':
+  case 'March':
+  case 'May':
+  case 'July':
+  case 'August':
+  case 'October':
+  case 'December':
+    result = `${month} has 31 days`;
+    break;
+  case 'April':
+  case 'June':
+  case 'September':
+  case 'November':
+    result = `${month} has 30 days`;
+    break;
+  case 'February':
+    if (leapYear === true) {
+      result = `${month} has 29 days`;
+    } else if (leapYear === false) {
+      result = `${month} has 28 days`;
+    }
+    break;
+  }
+  return result;
+}
+
+console.log(howManyDays('January'));
