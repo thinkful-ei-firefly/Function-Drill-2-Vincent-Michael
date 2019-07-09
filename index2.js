f9a3d42bf5ef1ca2fa5e671d311f20dd1d96d909
m-cal <iengineersoftware@gmail.com>
2019-07-09 08:42:22 -0400

    fixed to infinity problem

--- index2.js
+++ index2.js
@@ -1,20 +1,24 @@
 'use strict';
 
 function jediName(firstName, lastName) {
-  return  lastName.slice(0,3) + firstName.slice(0,2)
+  return  lastName.slice(0,3) + firstName.slice(0,2);
 }
 
+console.log(jediName('Harry', 'Potter'));
+
 function beyond(num){
-  if (num===Infinity||num===Number.NEGATIVE_INFINITY){
-    console.log('And Beyond')
+  if (num === Infinity || num === Number.NEGATIVE_INFINITY){
+    console.log('And Beyond');
   }
-  if (Infinity>num>0){
-    console.log('To Infinity')
+  if (Infinity > num && num > 0){
+    console.log('To Infinity');
   }
-  if (0>num>Number.NEGATIVE_INFINITY){
-    console.log('To Negative Infinity')
+  if (0 > num && num > Number.NEGATIVE_INFINITY){
+    console.log('To Negative Infinity');
   }
   if (num===0){
-    console.log('Staying Home')
+    console.log('Staying Home');
   }
 }
+
+beyond(0);
